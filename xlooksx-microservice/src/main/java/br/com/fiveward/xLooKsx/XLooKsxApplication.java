ccd1a8e2cd5f22fa3b3f933fa7ca1d892e1f48f9
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2019-05-25 10:47:35 -0300

    master
    commit da solution

---
+++ xlooksx-microservice/src/main/java/br/com/fiveward/xLooKsx/XLooKsxApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiveward.xLooKsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XLooKsxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XLooKsxApplication.class, args);
+	}
+
+}
